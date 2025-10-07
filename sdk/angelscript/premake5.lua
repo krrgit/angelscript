82bea2254662e2daba4f1f0d1fa5012a5183b508
kevin <89359821+krrgit@users.noreply.github.com>
2025-10-06 21:14:26 -0700

    Create premake5.lua

---
+++ sdk/angelscript/premake5.lua
@@ -0,0 +1,41 @@
+project "AngelScript"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"source/**.h",
+		"source/**.cpp",
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++11"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++11"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
